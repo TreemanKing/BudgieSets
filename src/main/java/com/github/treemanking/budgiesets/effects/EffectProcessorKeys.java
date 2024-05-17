b14ebc890ab0d484c6f92da74783a6a52fd9b62e
TreemanK <tristankrst@gmail.com>
2024-05-17 23:16:05 +1000

    feat: particle effects

--- src/main/java/com/github/treemanking/budgiesets/effects/EffectProcessorKeys.java
+++ src/main/java/com/github/treemanking/budgiesets/effects/EffectProcessorKeys.java
@@ -1,5 +1,9 @@
 package com.github.treemanking.budgiesets.effects;
 
+import org.bukkit.Material;
+
+import java.util.Map;
+
 public interface EffectProcessorKeys {
     String TYPE_KEY = "Type";
     String AMPLIFIER_KEY = "Amplifier";
@@ -14,7 +18,15 @@ public interface EffectProcessorKeys {
     String BOOLEAN_KEY = "Boolean";
     String AMOUNT_KEY = "Amount";
     String PARTICLE_KEY = "Particle";
-    String COLOR_KEY = "RGB";
+    String COLOR_KEY = "Hex-Color";
     String COUNT_KEY = "Count";
     String DATA_KEY = "Data";
+    String MATERIAL_KEY = "Material";
+    String INT_KEY = "Int";
+    String FLOAT_KEY = "Float";
+    String ARRIVAL_TIME_KEY = "Arrival-Time";
+    String FROM_COLOR_KEY = "From-Hex";
+    String TO_COLOR_KEY = "To-Hex";
+    String SIZE_KEY = "Size";
+    String OFFSET_KEY = "Offset";
 }
--- src/main/java/com/github/treemanking/budgiesets/effects/processors/ParticleProcessor.java
+++ src/main/java/com/github/treemanking/budgiesets/effects/processors/ParticleProcessor.java
@@ -6,9 +6,14 @@ import com.github.treemanking.budgiesets.effects.EffectProcessor;
 import com.github.treemanking.budgiesets.effects.EffectProcessorKeys;
 import com.github.treemanking.budgiesets.managers.armorsets.ArmorSetListener;
 import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.Vibration;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.MaterialData;
 
 import java.util.List;
 import java.util.Map;
@@ -23,18 +28,16 @@ public class ParticleProcessor implements EffectProcessor, EffectProcessorKeys {
                     if (equipStatus.equals(ArmorSetListener.EquipStatus.NOT_EQUIPPED)) return;
 
                     Particle particleType = Enum.valueOf(Particle.class, (String) particleMap.get(PARTICLE_KEY));
-                    int rgb = (int) particleMap.get(COLOR_KEY);
+                    String hex = (String) particleMap.get(COLOR_KEY);
                     int count = (int) particleMap.get(COUNT_KEY);
-                    System.out.println(particleMap.get(DATA_KEY));
-                    Class <?> particleDataType = particleType.getDataType();
+                    double offset = (double) particleMap.get(OFFSET_KEY);
+                    Map<?, ?> particleDataMap = (Map<?, ?>) particleMap.get(DATA_KEY);
 
                     List<String> conditions = (List<String>) particleMap.get("Conditions");
 
                     if (checkConditions(conditions, player)) {
-                        // TODO: Verify and check against the data field for each type of datafield
-                        spawnParticle(player, particleType, rgb, count, null);
+                        spawnParticle(player, particleType, count, offset, particleDataMap);
                     }
-
                 } else {
                     // Log an error or inform the user about the invalid configuration
                     BudgieSets.getBudgieSets().getLogger().warning("Invalid particle configuration: " + particleMap);
@@ -43,17 +46,30 @@ public class ParticleProcessor implements EffectProcessor, EffectProcessorKeys {
         }
     }
 
-    private void spawnParticle(Player player, Particle particle, int color, int count, Object data) {
+    private void spawnParticle(Player player, Particle particle, int count, double offset, Map<?, ?> dataMap) {
         ParticleBuilder particleBuilder = new ParticleBuilder(particle);
-        particleBuilder.location(player.getLocation()).count(count).color(Color.fromRGB(color));
+
+        if (dataMap == null || dataMap.isEmpty()) {
+            particleBuilder.location(player.getLocation())
+                    .count(count)
+                    .offset(offset, offset, offset)
+                    .spawn();
+        } else {
+            particleBuilder.location(player.getLocation())
+                    .count(count)
+                    .data(convertData(particle.getDataType(), dataMap, player))
+                    .offset(offset, offset, offset)
+                    .spawn();
+        }
     }
 
     private boolean validateParticleConfig(Map<?, ?> particleMap) {
         return particleMap.containsKey(PARTICLE_KEY)
                 && particleMap.containsKey(COUNT_KEY)
+                && particleMap.containsKey(OFFSET_KEY)
                 && isValidParticleEnum((String) particleMap.get(PARTICLE_KEY))
-                && particleMap.get(COLOR_KEY) instanceof Integer
-                && particleMap.get(COUNT_KEY) instanceof Integer;
+                && particleMap.get(COUNT_KEY) instanceof Integer
+                && particleMap.get(OFFSET_KEY) instanceof Double;
     }
 
     private boolean isValidParticleEnum(String type) {
@@ -61,8 +77,108 @@ public class ParticleProcessor implements EffectProcessor, EffectProcessorKeys {
             Enum.valueOf(Particle.class, type);
             return true;
         } catch (IllegalArgumentException exception) {
-            BudgieSets.getBudgieSets().getLogger().warning(type + "Not a valid sound.");
+            BudgieSets.getBudgieSets().getLogger().warning(type + "Not a valid particle.");
         }
         return false;
+    }
+
+    private Object convertData(Class<?> particleDataTypeClass, Map<?, ?> dataMap, Player player) {
+        if(dataMap == null) return null;
+
+        if (particleDataTypeClass.equals(MaterialData.class)) {
+            if (validateMaterialKey(dataMap)) {
+                return Material.getMaterial(dataMap.get(MATERIAL_KEY).toString().toUpperCase());
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        } else if (particleDataTypeClass.equals(BlockData.class)) {
+            if (validateMaterialKey(dataMap)) {
+                return Material.getMaterial(dataMap.get(MATERIAL_KEY).toString().toUpperCase()).createBlockData();
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        } else if (particleDataTypeClass.equals(Integer.class)) {
+            if (validateIntKey(dataMap)) {
+                return dataMap.get(INT_KEY);
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        } else if (particleDataTypeClass.equals(Float.class)) {
+            if (validateFloatKey(dataMap)) {
+                return (float) ((double) dataMap.get(FLOAT_KEY));
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        } else if (particleDataTypeClass.equals(Vibration.class)) {
+            if (validateArrivalTimeKey(dataMap)) {
+                return new Vibration(new Vibration.Destination.EntityDestination(player), (int) dataMap.get(ARRIVAL_TIME_KEY));
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        } else if (particleDataTypeClass.equals(Particle.DustTransition.class)) {
+            int[] fromColor = convertHexToRGB((String) dataMap.get(FROM_COLOR_KEY));
+            int[] toColor = convertHexToRGB((String) dataMap.get(TO_COLOR_KEY));
+            if (validateHexKey(dataMap)) {
+                return new Particle.DustTransition(
+                        Color.fromRGB(fromColor[0], fromColor[1], fromColor[2]),
+                        Color.fromRGB(toColor[0], toColor[1], toColor[2]),
+                        (float) dataMap.get(SIZE_KEY)
+                );
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        } else if (particleDataTypeClass.equals(ItemStack.class)) {
+            if (validateMaterialKey(dataMap)) {
+                return new ItemStack(Material.getMaterial(dataMap.get(MATERIAL_KEY).toString().toUpperCase()));
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        } else if (particleDataTypeClass.equals(Particle.DustOptions.class)) {
+            if (validateSizeKey(dataMap) && validateColorKey(dataMap)) {
+                int[] color = convertHexToRGB((String) dataMap.get(COLOR_KEY));
+                return new Particle.DustOptions(Color.fromRGB(color[0], color[1], color[2]), (float) ((double)dataMap.get(SIZE_KEY)));
+            }
+            BudgieSets.getBudgieSets().getLogger().warning("Invalid configuration. Please see the wiki on particle data.");
+            return null;
+        }
+        return null;
+    }
+
+
+    private boolean validateMaterialKey(Map<?, ?> map) {
+        return map.containsKey(MATERIAL_KEY)
+                && Material.getMaterial(map.get(MATERIAL_KEY).toString().toUpperCase()) != null;
+    }
+
+    private boolean validateFloatKey(Map<?, ?> map) {
+        return map.containsKey(FLOAT_KEY)
+                && map.get(FLOAT_KEY) instanceof Double;
+    }
+
+    private boolean validateIntKey(Map<?, ?> map) {
+        return map.containsKey(INT_KEY)
+                && map.get(INT_KEY) instanceof Integer;
+    }
+
+    private boolean validateArrivalTimeKey(Map<?, ?> map) {
+        return map.containsKey(ARRIVAL_TIME_KEY)
+                && map.get(ARRIVAL_TIME_KEY) instanceof Integer;
+    }
+
+    private boolean validateHexKey(Map<?, ?> map) {
+        return map.containsKey(FROM_COLOR_KEY)
+                && map.containsKey(TO_COLOR_KEY)
+                && map.get(FROM_COLOR_KEY) instanceof String
+                && map.get(TO_COLOR_KEY) instanceof String
+                && validateSizeKey(map);
+    }
+
+    private boolean validateSizeKey(Map<?, ?> map) {
+        return map.containsKey(SIZE_KEY);
+    }
+
+    private boolean validateColorKey(Map<?, ?> map) {
+        return map.containsKey(COLOR_KEY)
+                && map.get(COLOR_KEY) instanceof String;
     }
 }
